5d6519f32c9a6c0cb7ba97486e972228b0bb6184
jrvla <vladimir.artemj@gmail.com>
2025-11-01 21:32:23 +0500

    Added nav bar at pages

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,2 +1,8 @@
 const theme = localStorage.getItem("theme");
 console.log(theme);
+
+const exitButton = document.getElementById('exit_button');
+
+exitButton.addEventListener('click', function(){
+    window.location.href = 'login.html';
+});
